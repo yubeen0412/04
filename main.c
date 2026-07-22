95b57b8b6bdec7f232f094e9881dcaf1ea043b96
yubeen0412 <lyubeen07@gmail.com>
2026-07-22 11:50:30 +0900

    practice2

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
  
 int main () { 
-	int x, y, z, m; 
-	int a, b, c; 
-	x = 2; 
-	z = 1; 
-	a = 3; 
-	b = 4; 
-	c = 5; 
-	y = a*x*x+b*x+c;
-	m = (x+y+z)/3;
-
-	printf("y=%d, m=%d", y, m);
-}
+	int x, y; 
+	printf("input two integers :");
+	scanf("%i %i", &x, &y);
+	printf("+ result is %i\n", x + y);
+	printf("- result is %i\n", x - y);
+	printf("* result is %i\n", x * y);
+	printf("/ result is %i\n", x / y);
+	printf("%% result is %i\n", x % y);	
+	}
